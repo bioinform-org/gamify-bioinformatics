17cda13e2afff747c335ffd59aee66bb5665779a
Pavlo-Petrashevskyi <pavlopetrashevskyi@gmail.com>
2024-11-19 11:45:41 +0200

    feat: add routes and appropriate pages

---
+++ src/pages/ResetPasswordPage/ResetPasswordPage/ResetPasswordPage.tsx
@@ -0,0 +1,7 @@
+import './ResetPasswordPage.scss';
+
+interface Props {}
+
+export const ResetPasswordPage: React.FC<Props> = () => {
+  return (<div className="resetpasswordpage">Reset Password Page</div>);
+};
