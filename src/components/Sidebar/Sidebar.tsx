694f3d38f06eeadbb0580fc0ceff503f9a311f00
Pavlo-Petrashevskyi <pavlopetrashevskyi@gmail.com>
2024-11-28 22:50:19 +0200

    perf: add map method with nav links inside Sidebar.tsx; add position fixed to sidebar, so it will stay in one place; update navLinks pseudoclass styles

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,92 +4,47 @@ import classNames from "classnames";
 
 interface Props {}
 
+const navLinks = [
+  { name: 'Exercises', link: 'exercises'},
+  { name: 'Dashboard', link: 'dashboard'},
+  { name: 'My exercises', link: 'my-exercises'},
+  { name: 'Chats', link: 'chats'},
+  { name: 'Team management', link: 'team'},
+];
+
 export const Sidebar: React.FC<Props> = () => {
   return (
     <aside className="side-bar">
-      <div className="side-bar__header">
-        <img
-          className="side-bar__logo"
-          src="../../../public/images/Logo.svg"
-          alt="Bioinformatics logo"
-        />
+      <div className="side-bar__container">
+        <div className="side-bar__header">
+          <img
+            className="side-bar__logo"
+            src="../../../public/images/Logo.svg"
+            alt="Bioinformatics logo"
+          />
+        </div>
+        <nav className="side-bar__nav">
+          <ul className="side-bar__list">
+            {navLinks.map(({ name, link }) => (
+              <li 
+                key={name}
+                className="side-bar__item"
+              >
+                <NavLink
+                  className={({ isActive }: { isActive: boolean }) =>
+                    classNames(`side-bar__link side-bar__link--${link}`, {
+                      "side-bar__link--active": isActive,
+                    })
+                  }
+                  to={`/${link}`}
+                >
+                  {name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
-      <nav className="side-bar__nav">
-        <ul className="side-bar__list">
-          <li className="side-bar__item">
-            <NavLink
-              className={({ isActive }: { isActive: boolean }) =>
-                classNames("side-bar__link side-bar__link--exercises", {
-                  "side-bar__link--active": isActive,
-                })
-              }
-              to="/exercises"
-            >
-              Exercises
-            </NavLink>
-          </li>
-          <li className="side-bar__item">
-            <NavLink
-              className={({ isActive }: { isActive: boolean }) =>
-                classNames(
-                  "side-bar__link side-bar__link side-bar__link--dashboard",
-                  {
-                    "side-bar__link--active": isActive,
-                  }
-                )
-              }
-              to="/dashboard"
-            >
-              Dashboard
-            </NavLink>
-          </li>
-          <li className="side-bar__item">
-            <NavLink
-              className={({ isActive }: { isActive: boolean }) =>
-                classNames(
-                  "side-bar__link side-bar__link side-bar__link--my-exercises",
-                  {
-                    "side-bar__link--active": isActive,
-                  }
-                )
-              }
-              to="/my-exercises"
-            >
-              My exercises
-            </NavLink>
-          </li>
-          <li className="side-bar__item">
-            <NavLink
-              className={({ isActive }: { isActive: boolean }) =>
-                classNames(
-                  "side-bar__link side-bar__link side-bar__link--chats",
-                  {
-                    "side-bar__link--active": isActive,
-                  }
-                )
-              }
-              to="/chats"
-            >
-              Chats
-            </NavLink>
-          </li>
-          <li className="side-bar__item">
-            <NavLink
-              className={({ isActive }: { isActive: boolean }) =>
-                classNames(
-                  "side-bar__link side-bar__link side-bar__link--team",
-                  {
-                    "side-bar__link--active": isActive,
-                  }
-                )
-              }
-              to="/team"
-            >
-              Team management
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
     </aside>
   );
 };
