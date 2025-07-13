8831df49872ce09f2cea652f221cbbce73dadd93
nex1994 <bilicki1994@gmail.com>
2025-07-13 21:35:22 +0200

    BlockUser component styling

--- src/components/BlockedUsers/BlockedUsers.tsx
+++ src/components/BlockedUsers/BlockedUsers.tsx
@@ -14,12 +14,14 @@ export const BlockedUsers = () => {
       {blockedUsers.map((user, index) => {
         return (
           <div key={index} className="blocked-users__item">
-            <img
-              className="blocked-users__image"
-              src="/public/images/avatar_by_default.svg"
-              alt=""
-            />
-            <p className="blocked-users__name">{user.name}</p>
+            <div className="blocked-users__info">
+              <img
+                className="blocked-users__image"
+                src="/public/images/avatar_by_default.svg"
+                alt=""
+              />
+              <p className="blocked-users__name">{user.name}</p>
+            </div>
             <button className="blocked-users__button">Unblock</button>
           </div>
         );
