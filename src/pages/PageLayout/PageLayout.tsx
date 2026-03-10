3cab7ef406013acc179075f0c43910783a7ec62a
Damian Krawczyk <pirrat82@gmail.com>
2026-03-10 15:13:19 +0100

    Configure CI/CD redirects and fix dashboard layout

--- src/pages/PageLayout/PageLayout.tsx
+++ src/pages/PageLayout/PageLayout.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
-import { Sidebar } from "../../components/Sidebar";
-import "./PageLayout.scss";
 import { Link } from "react-router-dom";
+import { Sidebar } from "../../components/Sidebar";
 import { Loader } from "../../components/Loader/Loader";
 import { useAppDispatch } from "../../store/hooks";
 import { removeErrorMessageForUser } from "../../store/features/userSlice";
 import { removeToken } from "../../store/features/tokenSlice";
+import "./PageLayout.scss";
 
 type Props = {
   children: React.ReactNode;
@@ -21,90 +21,83 @@ export const PageLayout: React.FC<Props> = ({
   errorMessage,
 }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  // const token = useAppSelector(selectToken);
-  // const user = useAppSelector(selectUser);
   const dispatch = useAppDispatch();
-
-  // useEffect(() => {
-  //   if (!user.value && token.value) {
-  //     dispatch(getUser(token.value));
-  //   }
-  // }, [dispatch, token.value, user.value])
 
   return (
     <div className="page-layout">
-      <Sidebar />
+      <aside className="page-layout__sidebar-container">
+        <Sidebar />
+      </aside>
 
-      <div className="page-layout__content">
-        <div className="page-layout__header">
+      <div className="page-layout__right-panel">
+        
+        <header className="page-layout__header">
           <h2 className="page-layout__title">{pageTitle}</h2>
+          
           <div className="page-layout__user">
             <img
               className="page-layout__user-img"
               src="/images/avatar_by_default.svg"
-              alt=""
+              alt="User"
             />
             <button
               className="page-layout__user-btn"
               type="button"
               onClick={() => setIsMenuOpen(!isMenuOpen)}
-              //using setTimeout to give some time for a click on the link to be processed before closing
               onBlur={() => setTimeout(() => setIsMenuOpen(false), 240)}
             >
-              {/* {user.isLoading && <Loader shouldBeText={false}/>} */}
               Admin
             </button>
-          </div>
 
-          {isMenuOpen && (
-            <ul className="page-layout__user-menu">
-              <li className="page-layout__user-menu-item">
-                <Link
-                  className="page-layout__user-menu-btn page-layout__user-menu-btn--settings"
-                  to="/settings/account"
-                >
-                  Settings
-                </Link>
-              </li>
-              <li className="page-layout__user-menu-item">
-                <Link
-                  className="page-layout__user-menu-btn page-layout__user-menu-btn--help"
-                  to="/help-center"
-                >
-                  Help Center
-                </Link>
-              </li>
-              <li className="page-layout__user-menu-item">
-                <Link
-                  className="page-layout__user-menu-btn page-layout__user-menu-btn--logout"
-                  to=""
-                  onClick={() => {
-                    dispatch(removeToken());
-                    dispatch(removeErrorMessageForUser());
-                  }}
-                >
-                  Log Out
-                </Link>
-              </li>
-            </ul>
+            {isMenuOpen && (
+              <ul className="page-layout__user-menu">
+                <li className="page-layout__user-menu-item">
+                  <Link
+                    className="page-layout__user-menu-btn page-layout__user-menu-btn--settings"
+                    to="/settings/account"
+                  >
+                    Settings
+                  </Link>
+                </li>
+                <li className="page-layout__user-menu-item">
+                  <Link
+                    className="page-layout__user-menu-btn page-layout__user-menu-btn--help"
+                    to="/help-center"
+                  >
+                    Help Center
+                  </Link>
+                </li>
+                <li className="page-layout__user-menu-item">
+                  <Link
+                    className="page-layout__user-menu-btn page-layout__user-menu-btn--logout"
+                    to=""
+                    onClick={() => {
+                      dispatch(removeToken());
+                      dispatch(removeErrorMessageForUser());
+                    }}
+                  >
+                    Log Out
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </div>
+        </header>
+
+        <main className="page-layout__content-scroll">
+          {children}
+
+          {isLoading && (
+            <div className="page-layout__loader">
+              <p className="page-layout__loader-text">Loading...</p>
+              <Loader />
+            </div>
           )}
-        </div>
 
-        {children}
-
-        {isLoading && (
-          <div className="page-layout__loader">
-            <p className="page-layout__loader-text">
-              Please wait a moment while the page loads.
-            </p>
-
-            <Loader />
-          </div>
-        )}
-
-        {!isLoading && errorMessage && (
-          <div className="page-layout__error-message">{errorMessage}</div>
-        )}
+          {!isLoading && errorMessage && (
+            <div className="page-layout__error-message">{errorMessage}</div>
+          )}
+        </main>
       </div>
     </div>
   );
